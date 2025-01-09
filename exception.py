394aa514852a4d699a875d0ab0bb3efa14c96c1c
Nandan1041996 <mayurnandanwar@ghcl.co.in>
2025-01-09 11:55:34 +0530

    Update exception.py

--- exception.py
+++ exception.py
@@ -2,7 +2,23 @@
 class FileNotAvailable(Exception):
     'File Not Available.'
     def __init__(self):
-        self.msg = 'File Not Available.'
+        self.msg = 'File Not Available in google drive.'
+    def __str__(self):
+        return self.msg
+    
+
+class FolderNotAvailable(Exception):
+    'Folder Not Available.'
+    def __init__(self):
+        self.msg = 'In Google Drive Folder Not Available.'
+    def __str__(self):
+        return self.msg
+
+    
+class EmailExist(Exception):
+    'Email already exist.'
+    def __init__(self):
+        self.msg = 'Email already exist.'
     def __str__(self):
         return self.msg
 
